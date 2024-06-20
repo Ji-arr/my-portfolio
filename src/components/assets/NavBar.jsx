2c7f03970a0c383eb4ad4ca75c28f2c7ad20fb83
Ji-arr <gfor508@gmail.com>
2024-06-20 23:00:33 +0800

    asd

---
+++ src/components/assets/NavBar.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./NavBar.css";
+
+export const NavBar = () => {
+  const links = [
+    { index: 0, href: "banner", text: "Home" },
+    { index: 1, href: "project", text: "Project" },
+    { index: 2, href: "certificate", text: "Certificate" },
+    { index: 3, href: "contact", text: "contact" },
+  ];
+
+  const [colorChanged, setColorChanged] = useState(false);
+
+  const handleColorChange = () => {
+    setColorChanged(!colorChanged);
+  };
+
+  const handleOnClick = (index) => {
+    if (index) {
+      const element = document.getElementById(links[index].href);
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const toContact = () => {
+    const element = document.getElementById("contact");
+    element.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <div className="right">J</div>
+        <span className="with-line"></span>
+        <div className="left">J</div>
+      </div>
+      <div className={`links ${colorChanged ? "mobile" : ""}`}>
+        {links.map((link, index) => (
+          <a
+            href={`#${link.href}`}
+            key={index}
+            onClick={() => handleOnClick(index)}
+          >
+            <p className={`${link.text}`}>{link.text}</p>
+          </a>
+        ))}
+      </div>
+      <div className="navbutton">
+        <button onClick={toContact}>Contact Me</button>
+        <button className="toggler" onClick={handleColorChange}>
+          Links
+        </button>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/pages/Project.jsx
@@ -0,0 +1,53 @@
+import "./Project.css";
+export const Project = () => {
+  const projects = [
+    {
+      id: 1,
+      name: "Android App",
+      src: "mobile_app.jpg",
+      link: "https://drive.google.com/drive/folders/1h-KtMV5hRb0q4X6D-C-W6cKUqu2YWSlh?fbclid=IwAR3ofhxdKKgt8RoJJZFe-05M5VnLRlUbvuKb8MfJp1BbgT0r93QVJtzaysQ",
+    },
+    {
+      id: 2,
+      name: "Library Management System",
+      src: "lms.jpg",
+      link: "https://drive.google.com/drive/folders/1pIVG2uNvPNKOf58vlURZyeKQ5gGzDCJa?fbclid=IwAR3ofhxdKKgt8RoJJZFe-05M5VnLRlUbvuKb8MfJp1BbgT0r93QVJtzaysQ",
+    },
+    {
+      id: 3,
+      name: "Programming",
+      src: "prog.jpg",
+      link: "https://github.com/Ji-arr",
+    },
+    {
+      id: 4,
+      name: "Application Programming Interface",
+      src: "api.jpg",
+    },
+  ];
+  return (
+    <div className="project-div" id="project">
+      <div>
+        <h1>PROJECTS</h1>
+      </div>
+      <div className="flex-wrap">
+        {projects.map((project, index) => {
+          return (
+            <a href={project.link}>
+              <div
+                className="project-card"
+                style={{
+                  backgroundImage: `url(${process.env.PUBLIC_URL}/${project.src})`,
+                }}
+              >
+                <div className="overlay">
+                  <h1>{project.name}</h1>
+                </div>
+              </div>
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
